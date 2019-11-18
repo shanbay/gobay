d6dc4764692b9f88bfa9792c031ba4beebc3d1ad
Neng Wan <neng.wan@shanbay.com>
2019-11-18 17:14:21 +0800

    remove unused codes

--- encoderext/encoder.go
+++ encoderext/encoder.go
@@ -62,31 +62,6 @@ func getFieldsMap(fields []string) map[string]bool {
 	return fieldsMap
 }
 
-func mapDeepCopy(value map[string]interface{}) map[string]interface{} {
-	ncopy := deepCopy(value)
-	if nmap, ok := ncopy.(map[string]interface{}); ok {
-		return nmap
-	}
-	return nil
-}
-
-func deepCopy(value interface{}) interface{} {
-	if valueMap, ok := value.(map[string]interface{}); ok {
-		newMap := make(map[string]interface{})
-		for k, v := range valueMap {
-			newMap[k] = deepCopy(v)
-		}
-		return newMap
-	} else if valueSlice, ok := value.([]interface{}); ok {
-		newSlice := make([]interface{}, len(valueSlice))
-		for k, v := range valueSlice {
-			newSlice[k] = deepCopy(v)
-		}
-		return newSlice
-	}
-	return value
-}
-
 func (e *Encoder) EncodeMap(data interface{}, excludedFields []string) interface{} {
 	dataV, ok := data.(map[string]interface{})
 	if !ok {
