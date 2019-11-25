d0e5a79bc6f4d4922609060e527763d46cb04fdb
sljeff <shanliangjeff@qq.com>
2019-11-25 13:52:40 +0800

    feat: idgen

---
+++ seqgenext/ext.go
@@ -0,0 +1,157 @@
+package seqgenext
+
+/*
+核心原理是根据时间戳的自增特性, 加上自增数解决统一毫秒下的并发问题
+虽然时间戳是一个 64 位整数, 但实际远未达到该长度
+使用 48 位存储毫秒时间, 自 BEGINNING_TIMESTAMP 后 2 ** 48 / 1000 / 3600 / 24 = 3257812.2304474073 天后此算法才会失效
+
+"{:064b}".format(((int(time.time() * 1000) - BEGINNING_TIMESTAMP) << TIMESTAMP_SHIFT) + MAX_SEQUENCE)
+'0000000010110101100111101101011000010111010001100000000000000000'
+*/
+import (
+	"fmt"
+	"sync"
+
+	"github.com/go-redis/redis/v7"
+	"github.com/shanbay/gobay"
+)
+
+const (
+	TIMESTAMP_SHIFT = 15
+	MAX_STEP        = 1 << (TIMESTAMP_SHIFT - 2)
+	// 空一位是为了避免 incrby step 超出 14 位导致自增溢出
+	MAX_SEQUENCE = 1 << (TIMESTAMP_SHIFT - 2)
+	// 起始时间的作用是避免时间过早的达到边界
+	// 1514764800 是 UTC 时间 2018-01-01 00:00:00
+	BEGINNING_TIMESTAMP = 1514764800
+	// 由于 increment 跟时间无关, 某一毫秒内 increment 可能出现 ... MAX_SEQUENCE, 1, 2, ... 的序列, sequence 时间递增精度只能为毫秒
+	SCRIPT = `
+local sq = redis.call('incrby', KEYS[1], ARGV[2])
+if(sq>tonumber(ARGV[1]))
+then
+redis.call('del', KEYS[1])
+end
+local t =  redis.call('time')
+return {sq, tonumber(t[1]), tonumber(t[2])}
+`
+)
+
+type SequenceGeneratorExt struct {
+	redisClient  *redis.Client
+	app          *gobay.Application
+	NS           string
+	RedisExtName gobay.Key
+	SequenceBase uint64
+	SequenceKey  string
+}
+
+// Init implements Extension interface
+func (d *SequenceGeneratorExt) Init(app *gobay.Application) error {
+	config := app.Config()
+	if d.NS != "" {
+		config = config.Sub(d.NS)
+	}
+	d.redisClient = app.Get(d.RedisExtName).Object().(*redis.Client)
+	d.SequenceBase = config.GetUint64("sequence_base")
+	d.SequenceKey = config.GetString("sequence_key")
+	return nil
+}
+
+// Object implements Extension interface
+func (d *SequenceGeneratorExt) Object() interface{} {
+	return nil
+}
+
+// Application implements Extension interface
+func (d *SequenceGeneratorExt) Application() *gobay.Application {
+	return d.app
+}
+
+// Close implements Extension interface
+func (d *SequenceGeneratorExt) Close() error {
+	return nil
+}
+
+// 当 step > 1 时, 即分配了一批 sequence, 可以使用 (sequence - step, sequence] 间的 sequence,
+// 注意此时在分布式环境下 sequence 并不能保证随着时间递增
+func (g *SequenceGeneratorExt) getSequence(step uint64) (uint64, error) {
+	if step < 1 || step > MAX_SEQUENCE {
+		return 0, fmt.Errorf("step should not less than 1 or greater than MAX_STEP(%d)", MAX_STEP)
+	}
+	cmd := g.redisClient.Eval(SCRIPT, []string{g.SequenceKey}, MAX_SEQUENCE, step)
+	result, err := cmd.Result()
+	if err != nil {
+		return 0, err
+	}
+	values := result.([]interface{})
+	increment := uint64(values[0].(int64))
+	currentSeconds := uint64(values[1].(int64))
+	currentMicroseconds := uint64(values[2].(int64))
+	shiftedMillisecond := ((currentSeconds-BEGINNING_TIMESTAMP)*1000 + currentMicroseconds/1000) << TIMESTAMP_SHIFT
+	sequence := shiftedMillisecond + increment + g.SequenceBase
+	return sequence, nil
+}
+
+func (g *SequenceGeneratorExt) GetSequence() (uint64, error) {
+	return g.getSequence(1)
+}
+
+// 批量生成 sequence, 减少 redis 请求
+// count: 需要生成的 sequence 数量
+// batch_size: 单词请求 redis 取得的 sequence 数量,
+// 调用 redis 生成 sequence 的 QPS 不能超过 (MAX_SEQUENCE / batch_size + 1) * 1000 => batch_size = MAX_SEQUENCE / (MAX_QPS / 1000 - 1)
+// 以单台 redis 极限 4w QPS 计算(单条命令 25us), batch_size = 210.0512820513
+func (g *SequenceGeneratorExt) GetSequences(count, batchSize uint64) *Sequences {
+	return &Sequences{
+		g:         g,
+		restCount: count,
+		batchSize: batchSize,
+	}
+}
+
+type Sequences struct {
+	g                    *SequenceGeneratorExt
+	restCount, batchSize uint64
+
+	mux             sync.Mutex
+	lastMaxSequence uint64
+	lastSequence    uint64
+
+	// 用于减少内存分配次数
+	step uint64
+	err  error
+}
+
+func (s *Sequences) HasNext() bool {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	return s.restCount > 0
+}
+
+func (s *Sequences) Next() (uint64, error) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+
+	if s.restCount == 0 {
+		return s.lastSequence, nil
+	}
+	if s.lastSequence < s.lastMaxSequence {
+		s.lastSequence += 1
+		s.restCount -= 1
+		return s.lastSequence, nil
+	}
+	if s.restCount < s.batchSize {
+		s.step = s.restCount
+	} else {
+		s.step = s.batchSize
+	}
+
+	s.lastMaxSequence, s.err = s.g.getSequence(s.step)
+	if s.err != nil {
+		s.restCount = 0
+		return 0, s.err
+	}
+	s.lastSequence = s.lastMaxSequence - s.step + 1
+	s.restCount -= 1
+	return s.lastSequence, nil
+}
---
+++ seqgenext/ext_test.go
@@ -0,0 +1,91 @@
+package seqgenext
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/go-redis/redis/v7"
+)
+
+func TestSequenceGenerator(t *testing.T) {
+	client := redis.NewClient(&redis.Options{Addr: "127.0.0.1:6379"})
+	defer client.Close()
+	g := SequenceGeneratorExt{
+		redisClient:  client,
+		SequenceBase: 0,
+		SequenceKey:  "test_key",
+	}
+	_, err := g.getSequence(MAX_SEQUENCE + 1)
+	expectedErr := fmt.Errorf("step should not less than 1 or greater than MAX_STEP(%d)", MAX_STEP)
+	if err == nil || err.Error() != expectedErr.Error() {
+		t.Errorf("err `%s` is not expected, expected `%s`", err, expectedErr)
+	}
+
+	count := 10
+	sequencesSet := make(map[uint64]struct{}, 10)
+	for i := 0; i < count; i++ {
+		sequence, err := g.GetSequence()
+		if err != nil {
+			t.Fatal(err)
+		}
+		sequencesSet[sequence] = struct{}{}
+	}
+	if len(sequencesSet) != count {
+		t.Fatalf("GetSequence count %d, expected %d", len(sequencesSet), count)
+	}
+
+	sequences := g.GetSequences(uint64(count), 3)
+	for sequences.HasNext() {
+		sequence, err := sequences.Next()
+		if err != nil {
+			t.Fatal(err)
+		}
+		sequencesSet[sequence] = struct{}{}
+	}
+	if len(sequencesSet) != count*2 {
+		t.Fatalf("GetSequences count %d, expected %d", len(sequencesSet)-count, count)
+	}
+
+	sequence, err := sequences.Next()
+	if err != nil {
+		t.Fatal(err)
+	}
+	sequencesSet[sequence] = struct{}{}
+	if len(sequencesSet) != count*2 {
+		t.Fatalf("GetSequences count %d, expected %d", len(sequencesSet)-count, count)
+	}
+}
+
+func BenchmarkSequenceGenerator_GetSequence(b *testing.B) {
+	client := redis.NewClient(&redis.Options{Addr: "127.0.0.1:6379"})
+	defer client.Close()
+	g := SequenceGeneratorExt{
+		redisClient:  client,
+		SequenceBase: 0,
+		SequenceKey:  "test_key",
+	}
+	for i := 0; i < b.N; i++ {
+		if _, err := g.GetSequence(); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkSequenceGenerator_GetSequences_100(b *testing.B) {
+	client := redis.NewClient(&redis.Options{Addr: "127.0.0.1:6379"})
+	defer client.Close()
+	g := SequenceGeneratorExt{
+		redisClient:  client,
+		SequenceBase: 0,
+		SequenceKey:  "test_key",
+	}
+	count := 100
+	for i := 0; i < b.N; i++ {
+		sequences := g.GetSequences(uint64(count), 100)
+		for sequences.HasNext() {
+			if _, err := sequences.Next(); err != nil {
+				b.Fatal(err)
+			}
+		}
+	}
+}
