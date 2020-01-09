7e8aa447407856739a190a12f31948701accf976
sljeff <shanliangjeff@qq.com>
2020-01-09 17:06:29 +0800

    seqgen: redis client

--- extensions/seqgenext/ext.go
+++ extensions/seqgenext/ext.go
@@ -37,8 +37,12 @@ return {sq, tonumber(t[1]), tonumber(t[2])}
 `
 )
 
+type redisClient interface {
+	Eval(script string, keys []string, args ...interface{}) *redis.Cmd
+}
+
 type SequenceGeneratorExt struct {
-	redisClient  *redis.Client
+	redisClient  redisClient
 	app          *gobay.Application
 	NS           string
 	RedisExtName gobay.Key
@@ -80,7 +84,7 @@ func (g *SequenceGeneratorExt) getSequence(step uint64) (uint64, error) {
 		return 0, fmt.Errorf("step should not less than 1 or greater than MAX_STEP(%d)", maxStep)
 	}
 	if g.redisClient == nil {
-		g.redisClient = g.app.Get(g.RedisExtName).Object().(*redis.Client)
+		g.redisClient = g.app.Get(g.RedisExtName).Object().(redisClient)
 	}
 	cmd := g.redisClient.Eval(luaScript, []string{g.SequenceKey}, maxSequence, step)
 	result, err := cmd.Result()
