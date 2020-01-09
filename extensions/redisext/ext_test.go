9a7022939e8427e0add3fc6c11ccc77ffb30ad79
Neng Wan <neng.wan@shanbay.com>
2020-01-09 16:04:07 +0800

    add test cases for redisExt

---
+++ extensions/redisext/ext_test.go
@@ -0,0 +1,45 @@
+package redisext_test
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/shanbay/gobay"
+	"github.com/shanbay/gobay/extensions/redisext"
+)
+
+func ExampleRedisExt_Set() {
+	redis := &redisext.RedisExt{NS: "redis_"}
+	exts := map[gobay.Key]gobay.Extension{
+		"redis": redis,
+	}
+	if _, err := gobay.CreateApp("../../testdata/", "testing", exts); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var key = "redisKey"
+	err := redis.Set(key, "hello", 10*time.Second).Err()
+	fmt.Println(err)
+	res, err := redis.Get("redisKey").Result()
+	fmt.Println(res, err)
+	// Output:
+	// <nil>
+	// hello <nil>
+}
+
+func ExampleRedisExt_AddPrefix() {
+	redis := &redisext.RedisExt{NS: "redis_"}
+	exts := map[gobay.Key]gobay.Extension{
+		"redis": redis,
+	}
+	if _, err := gobay.CreateApp("../../testdata/", "testing", exts); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	prefixKey := redis.AddPrefix("testRawKey")
+	fmt.Println(prefixKey)
+	// Output:
+	// github-redis.testRawKey
+}
