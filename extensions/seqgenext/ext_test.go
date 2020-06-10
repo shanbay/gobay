f7a05d8ea2683761f7c966559da907e2215c1b42
sljeff <shanliangjeff@qq.com>
2020-06-10 16:37:21 +0800

    修复 seqgen

--- extensions/seqgenext/ext_test.go
+++ extensions/seqgenext/ext_test.go
@@ -1,21 +1,32 @@
 package seqgenext
 
 import (
+	"context"
 	"fmt"
 	"testing"
 
-	"github.com/go-redis/redis"
+	"github.com/shanbay/gobay"
+	"github.com/shanbay/gobay/extensions/redisext"
+)
+
+var (
+	ctx    = context.Background()
+	app, _ = gobay.CreateApp("../../testdata/", "testing", map[gobay.Key]gobay.Extension{
+		"redis": &redisext.RedisExt{
+			NS: "redis_",
+		},
+		"seqgen": &SequenceGeneratorExt{
+			NS:           "seqgen_",
+			RedisExtName: "redis",
+			SequenceBase: 0,
+			SequenceKey:  "test_key",
+		},
+	})
 )
 
 func TestSequenceGenerator(t *testing.T) {
-	client := redis.NewClient(&redis.Options{Addr: "127.0.0.1:6379"})
-	defer client.Close()
-	g := SequenceGeneratorExt{
-		redisClient:  client,
-		SequenceBase: 0,
-		SequenceKey:  "test_key",
-	}
-	_, err := g.getSequence(maxSequence + 1)
+	g := app.Get("seqgen").Object().(*SequenceGeneratorExt)
+	_, err := g.getSequence(ctx, maxSequence+1)
 	expectedErr := fmt.Errorf("step should not less than 1 or greater than MAX_STEP(%d)", maxStep)
 	if err == nil || err.Error() != expectedErr.Error() {
 		t.Errorf("err `%s` is not expected, expected `%s`", err, expectedErr)
@@ -24,7 +35,7 @@ func TestSequenceGenerator(t *testing.T) {
 	count := 10
 	sequencesSet := make(map[uint64]struct{}, 10)
 	for i := 0; i < count; i++ {
-		sequence, err := g.GetSequence()
+		sequence, err := g.GetSequence(ctx)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -36,7 +47,7 @@ func TestSequenceGenerator(t *testing.T) {
 
 	sequences := g.GetSequences(uint64(count), 3)
 	for sequences.HasNext() {
-		sequence, err := sequences.Next()
+		sequence, err := sequences.Next(ctx)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -46,7 +57,7 @@ func TestSequenceGenerator(t *testing.T) {
 		t.Fatalf("GetSequences count %d, expected %d", len(sequencesSet)-count, count)
 	}
 
-	sequence, err := sequences.Next()
+	sequence, err := sequences.Next(ctx)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -57,33 +68,21 @@ func TestSequenceGenerator(t *testing.T) {
 }
 
 func BenchmarkSequenceGenerator_GetSequence(b *testing.B) {
-	client := redis.NewClient(&redis.Options{Addr: "127.0.0.1:6379"})
-	defer client.Close()
-	g := SequenceGeneratorExt{
-		redisClient:  client,
-		SequenceBase: 0,
-		SequenceKey:  "test_key",
-	}
+	g := app.Get("seqgen").Object().(*SequenceGeneratorExt)
 	for i := 0; i < b.N; i++ {
-		if _, err := g.GetSequence(); err != nil {
+		if _, err := g.GetSequence(ctx); err != nil {
 			b.Fatal(err)
 		}
 	}
 }
 
 func BenchmarkSequenceGenerator_GetSequences_100(b *testing.B) {
-	client := redis.NewClient(&redis.Options{Addr: "127.0.0.1:6379"})
-	defer client.Close()
-	g := SequenceGeneratorExt{
-		redisClient:  client,
-		SequenceBase: 0,
-		SequenceKey:  "test_key",
-	}
+	g := app.Get("seqgen").Object().(*SequenceGeneratorExt)
 	count := 100
 	for i := 0; i < b.N; i++ {
 		sequences := g.GetSequences(uint64(count), 100)
 		for sequences.HasNext() {
-			if _, err := sequences.Next(); err != nil {
+			if _, err := sequences.Next(ctx); err != nil {
 				b.Fatal(err)
 			}
 		}
